19a0997f9f25aeefcff801ff85e612ec60540db7
Robin Hughes <robin22391@hotmail.com>
2018-11-23 16:45:09 +0000

    populates selectView dropdown

--- client/src/app.js
+++ client/src/app.js
@@ -1,6 +1,6 @@
 const Countries = require('./models/countries.js');
 // const CountryView = require('./views/country_view.js');
-// const SelectView = require('./views/select_view.js');
+const SelectView = require('./views/select_view.js');
 // const  = require('./views/select_view.js');
 // const  = require('./views/country_view.js');
 
@@ -8,6 +8,10 @@ document.addEventListener('DOMContentLoaded', () => {
 // document.querySelector('');
 // const  = new ();
 //  .bindEvents();
+
+const selectDropdown = document.querySelector('.country-select');
+const selectView = new SelectView(selectDropdown);
+selectView.bindEvents();
 
 const countries = new Countries();
 countries.getData();
--- client/src/views/select_view.js
+++ client/src/views/select_view.js
@@ -1,1 +1,23 @@
 const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function (selectElement) {
+  this.selectElement = selectElement;
+}
+
+SelectView.prototype.bindEvents = function () {
+  PubSub.subscribe('Countries:country_names_ready', (evt) => {
+    this.populate(evt.detail);
+  })
+};
+
+SelectView.prototype.populate = function (countryNames) {
+  countryNames.forEach((countryName) => {
+    const option = document.createElement('option');
+    option.classList.add('country-select-option');
+    option.value = countryName;
+    option.textContent = countryName;
+    this.selectElement.appendChild(option);
+  });
+};
+
+module.exports = SelectView;
