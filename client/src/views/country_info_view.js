acf64a618569e409b282d356212a26b7a888f497
louisemeney <louisemeney@gmail.com>
2018-11-28 10:38:49 +0000

    clears existing country info when new country is selected

--- client/src/views/country_info_view.js
+++ client/src/views/country_info_view.js
@@ -6,33 +6,24 @@ const CountryInfoView = function (container) {
 
 CountryInfoView.prototype.bindEvents = function () {
   PubSub.subscribe('Countries:selected-country-ready', (evt) => {
-    // this.clearCountry();
     this.render(evt.detail);
   });
 }
 
 CountryInfoView.prototype.render = function (country) {
-  // const countryInfoWrapper = document.createElement('div');
-  // countryInfoWrapper.classList.add('country-info-wrapper');
-  // const capital = document.createElement('p');
-  // capital.classList.add('country-info-capital');
-  // const population = document.createElement('p');
-  // population.classList.add('country-info-population');
-  // capital.textContent = country.capital;
-  // population.textContent = country.population;
-  // countryInfoWrapper.appendChild(capital);
-  // countryInfoWrapper.appendChild(population);
-  // this.container.appendChild(countryInfoWrapper);
   const capitalParagraph = document.createElement('p');
   capitalParagraph.textContent = `The capital city of ${country.name} is ${country.capital}`;
   const capitalContainer = document.querySelector('.d');
+  capitalContainer.innerHTML = "";
   capitalContainer.appendChild(capitalParagraph);
   const populationParagraph = document.createElement('p');
   const populationContainer = document.querySelector('.e');
+  populationContainer.innerHTML = "";
   populationParagraph.textContent = `The population of ${country.name} is ${country.population}`;
   populationContainer.appendChild(populationParagraph);
   const regionParagraph = document.createElement('p');
   const regionContainer = document.querySelector('.f');
+  regionContainer.innerHTML = "";
   regionParagraph.textContent = `${country.name} is part of the ${country.region} region`;
   regionContainer.appendChild(regionParagraph);
 };
