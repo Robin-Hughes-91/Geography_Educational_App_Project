2f39071fafabc1a0b20c9ccdfb1a09439f6db5c9
louisemeney <louisemeney@gmail.com>
2018-11-28 07:28:56 +0000

    renders population to page

--- client/src/views/country_info_view.js
+++ client/src/views/country_info_view.js
@@ -12,17 +12,37 @@ CountryInfoView.prototype.bindEvents = function () {
 }
 
 CountryInfoView.prototype.render = function (country) {
-  const countryInfoWrapper = document.createElement('div');
-  countryInfoWrapper.classList.add('country-info-wrapper');
-  const capital = document.createElement('p');
-  capital.classList.add('country-info-capital');
-  const population = document.createElement('p');
-  population.classList.add('country-info-population');
-  capital.textContent = country.capital;
-  population.textContent = country.population;
-  countryInfoWrapper.appendChild(capital);
-  countryInfoWrapper.appendChild(population);
-  this.container.appendChild(countryInfoWrapper);
+  // const countryInfoWrapper = document.createElement('div');
+  // countryInfoWrapper.classList.add('country-info-wrapper');
+  // const capital = document.createElement('p');
+  // capital.classList.add('country-info-capital');
+  // const population = document.createElement('p');
+  // population.classList.add('country-info-population');
+  // capital.textContent = country.capital;
+  // population.textContent = country.population;
+  // countryInfoWrapper.appendChild(capital);
+  // countryInfoWrapper.appendChild(population);
+  // this.container.appendChild(countryInfoWrapper);
+  const capitalParagraph = document.createElement('p');
+  capitalParagraph.textContent = `The capital city of ${country.name} is ${country.capital}`;
+  const capitalContainer = document.querySelector('.d');
+  capitalContainer.appendChild(capitalParagraph);
+  const populationParagraph = document.createElement('p');
+  const populationContainer = document.querySelector('.e');
+  console.log('container for population', populationContainer);
+  populationParagraph.textContent = `The population of ${country.name} is ${country.population}`;
+  console.log('population paragraph', populationParagraph);
+  populationContainer.appendChild(populationParagraph);
+};
+
+// CountryInfoView.prototype.populateLanguageList = function (languages, list) {
+//   const languagesContainer = document.querySelector('f');
+//   languages.forEach((language) => {
+//     const languageListItem = document.createElement('li');
+//     listItem.textContent = language.name;
+//     console.log('language', language.name);
+//     languagesContainer.appendChild(listItem);
+//   });
 };
 
 
